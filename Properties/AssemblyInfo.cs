2ec29d1affff9b971a16224dbadaeffb1f64a120
LittleWolfStorm <116356445+WillWunderhorn@users.noreply.github.com>
2026-05-18 12:54:27 +0300

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using MelonLoader;
 using AfflictionsAndBuffs;
 
-[assembly: MelonInfo(typeof(AfflictionsAndBuffs.Core), "AfflictionsAndBuffs", "1.0.0", "LittleWolfStorm", null)]
+[assembly: MelonInfo(typeof(AfflictionsAndBuffs.Core), "AfflictionsAndBuffs", "1.5.0", "LittleWolfStorm", null)]
 [assembly: MelonGame("Hinterland", "TheLongDark")]
 
